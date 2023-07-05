2d566857a4a2b0c1bbebe001051dc55e17d97c9f
IvanMartinezArias <131498634+IvanMartinezArias@users.noreply.github.com>
2023-07-05 11:55:43 -0500

    se crea ejemplo de modificadores

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import org.example.modelos.Usuario;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    Integer edad=25;
+    //los objetos son variables, pero especiales
+        Universidad objetoUniversidad=new Universidad();
+        Usuario objetoUsuario=new Usuario(25,"juan","234",4,"juan@gmail.com");
+
+        //creo un objeto para acceder o modificar los atributos  o metodos de ese objeto
+        System.out.println(objetoUniversidad.nombres);
+        System.out.println(objetoUsuario.nombres);
     }
 }
---
+++ src/main/java/org/example/Universidad.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Universidad {
+    private String nombre;
+
+    Universidad() {
+    }
+
+    public Universidad(String nombre) {
+        this.nombre = nombre;
+    }
+}
